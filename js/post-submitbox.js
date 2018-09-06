48450ea9bd63a365afad4ae14aa6998075064b90
Erik <erikdmitchell@gmail.com>
2018-09-06 12:32:25 -0400

    removed checkbox js trigger, edit btn controls it

--- js/post-submitbox.js
+++ js/post-submitbox.js
@@ -2,19 +2,6 @@ jQuery(document).ready(function($) {
    
     // initial show of textbox.
     var orgTextValue = $('#pickle-twitter-text').val();
-    
-    if ($('.misc-pub-pickle-twitter input.pickle-post-to-twitter').is(':checked')) {
-        $('#pickle-twitter-text-wrap').show();
-    }
-    
-    // when checkbox changes, show/hide textbox.
-    $('.misc-pub-pickle-twitter input.pickle-post-to-twitter').on('change', function() {
-        if ($(this).is(':checked')) {
-            $('#pickle-twitter-text-wrap').show();
-        } else {
-            $('#pickle-twitter-text-wrap').hide();
-        }
-    });
 
     // when edit button clicked, show/hide textbox.
     $('.edit-pickle-twitter-text').on('click', function(e) {
